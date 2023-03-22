4f105ddc960d92623eabdb97fb1997e3ad782acf
Ribeiro <karen.ribeiro@capgemini.com>
2023-03-22 10:17:07 -0300

    first commit

---
+++ src/main/java/com/DesignPatterns/JavaMongo/JavaMongoApplication.java
@@ -0,0 +1,14 @@
+package com.DesignPatterns.JavaMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMongoApplication.class, args);
+	}
+
+	
+}
---
+++ src/test/java/com/DesignPatterns/JavaMongo/JavaMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DesignPatterns.JavaMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
