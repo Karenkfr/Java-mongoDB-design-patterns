093fe0c13cf045fb5266f89f21e62204cb04d4bd
Karen Ferraz <karen.ferrazribeiro@hotmail.com>
2023-03-23 14:39:51 -0300

    Obtendo um post por id

---
+++ src/main/java/com/DesignPatterns/JavaMongo/resources/PostResource.java
@@ -0,0 +1,41 @@
+package com.DesignPatterns.JavaMongo.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.DesignPatterns.JavaMongo.domain.Post;
+import com.DesignPatterns.JavaMongo.domain.User;
+import com.DesignPatterns.JavaMongo.dto.UserDTO;
+import com.DesignPatterns.JavaMongo.services.PostService;
+import com.DesignPatterns.JavaMongo.services.UserService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+	
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		
+		Post obj = postService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+}
---
+++ src/main/java/com/DesignPatterns/JavaMongo/services/PostService.java
@@ -0,0 +1,30 @@
+package com.DesignPatterns.JavaMongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.DesignPatterns.JavaMongo.domain.Post;
+import com.DesignPatterns.JavaMongo.domain.User;
+import com.DesignPatterns.JavaMongo.dto.UserDTO;
+import com.DesignPatterns.JavaMongo.repository.PostRepository;
+import com.DesignPatterns.JavaMongo.repository.UserRepository;
+import com.DesignPatterns.JavaMongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+}
