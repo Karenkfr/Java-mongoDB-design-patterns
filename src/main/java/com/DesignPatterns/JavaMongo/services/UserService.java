f5d01b86cbb50063010c27d0751348141a7cd3f1
Karen Ferraz <karen.ferrazribeiro@hotmail.com>
2023-03-23 10:18:55 -0300

    delecao de usuario com delete funcionando

--- src/main/java/com/DesignPatterns/JavaMongo/services/UserService.java
+++ src/main/java/com/DesignPatterns/JavaMongo/services/UserService.java
@@ -30,6 +30,10 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
