2b18d43bc567d82a55debbd8143b670878bf1ffc
Karen Ferraz <karen.ferrazribeiro@hotmail.com>
2023-03-23 09:24:08 -0300

    Obtendo usuario por id funcionando

--- src/main/java/com/DesignPatterns/JavaMongo/resources/UserResource.java
+++ src/main/java/com/DesignPatterns/JavaMongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,15 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	
 	
 	
 }
---
+++ src/main/java/com/DesignPatterns/JavaMongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.DesignPatterns.JavaMongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
