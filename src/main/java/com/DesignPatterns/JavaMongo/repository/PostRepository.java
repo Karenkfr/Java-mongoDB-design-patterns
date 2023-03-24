7a65e91826da39ea01a63f1f9b959f87b903349f
Ribeiro <karen.ribeiro@capgemini.com>
2023-03-24 09:51:57 -0300

    Consulta com vários critérios funcionando

--- src/main/java/com/DesignPatterns/JavaMongo/repository/PostRepository.java
+++ src/main/java/com/DesignPatterns/JavaMongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.DesignPatterns.JavaMongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,12 +12,11 @@ import com.DesignPatterns.JavaMongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
-	@Query("{'title': {$regex: ?0, $options:'i'}}")
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-	
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
-	
-	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/DesignPatterns/JavaMongo/resources/PostResource.java
+++ src/main/java/com/DesignPatterns/JavaMongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.DesignPatterns.JavaMongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,20 @@ public class PostResource {
 		text = URL.decodeParam(text);
 		List<Post> list = postService.findByTitle(text);
 		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@GetMapping("/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+		
 	}
 	
 	
