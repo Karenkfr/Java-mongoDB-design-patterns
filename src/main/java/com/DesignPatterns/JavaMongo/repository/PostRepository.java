11e733aaf384632d4bb8ed69e3c4a1a04f7a68e2
Ribeiro <karen.ribeiro@capgemini.com>
2023-03-24 08:53:27 -0300

    consulta simples com Query

--- src/main/java/com/DesignPatterns/JavaMongo/repository/PostRepository.java
+++ src/main/java/com/DesignPatterns/JavaMongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.DesignPatterns.JavaMongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.DesignPatterns.JavaMongo.domain.Post;
@@ -10,6 +11,12 @@ import com.DesignPatterns.JavaMongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{'title': {$regex: ?0, $options:'i'}}")
+	List<Post> searchTitle(String text);
+	
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
 	
 }
