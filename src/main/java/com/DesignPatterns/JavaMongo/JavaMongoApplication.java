2c962ffdb2aa78e0c53fda6d64a1bbad5fd82dca
Ribeiro <karen.ribeiro@capgemini.com>
2023-03-27 11:03:39 -0300

    Alterações pom.xml e reade.me

--- src/main/java/com/DesignPatterns/JavaMongo/JavaMongoApplication.java
+++ src/main/java/com/DesignPatterns/JavaMongo/JavaMongoApplication.java
@@ -2,9 +2,10 @@ package com.DesignPatterns.JavaMongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaMongoApplication {
+public class JavaMongoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaMongoApplication.class, args);
